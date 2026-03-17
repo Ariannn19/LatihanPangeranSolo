c426a99a3c072db4e0fe4a08460d062aab9867a4
Kasfa Rakha Faza <kasfa280@gmail.com>
2026-03-17 23:33:18 +0700

    ganti scanf menjadi fgets

--- find.c
+++ find.c
@@ -9,16 +9,17 @@ void findword(char text[][100], int lineCount) {
     int found = 0;
 
     printf("Masukkan kata yang ingin dicari: ");
-    scanf("%49s", keyword);  // lebih aman (hindari overflow)
-    getchar(); // bersihin newline dari buffer
+
+    fgets(keyword, sizeof(keyword), stdin);
+    keyword[strcspn(keyword, "\n")] = '\0';
 
     for(int i = 0; i < lineCount; i++) {
 
-        // DEBUG (boleh dihapus nanti)
-        // printf("DEBUG: [%s]\n", text[i]);
+        // DEBUG
+       // printf("DEBUG text[%d]: [%s]\n", i, text[i]);
+        //printf("DEBUG keyword: [%s]\n", keyword);
 
         if(strstr(text[i], keyword) != NULL) {
-
             printf("Ditemukan di baris %d: %s\n", i + 1, text[i]);
             found = 1;
         }
