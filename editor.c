36f675c974ff240e5c748b6768b079afe761f83d
Kasfa Rakha Faza <kasfa280@gmail.com>
2026-03-17 23:07:44 +0700

    ganti sintak

--- editor.c
+++ editor.c
@@ -20,6 +20,7 @@ void inputtext(){
     while(lineCount < MAX_LINES) {
 
         fgets(text[lineCount], MAX_COLS, stdin);
+        text[lineCount][strcspn(text[lineCount], "\n")] = '\0';
 
         if(strncmp(text[lineCount], "END", 3) == 0) {
             break;
